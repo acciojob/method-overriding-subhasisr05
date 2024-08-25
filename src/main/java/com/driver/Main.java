c6cf414b706ac92e8124679041aab4dec5048637
subhasis1904 <166941066+subhasis1904@users.noreply.github.com>
2024-08-25 13:47:52 +0530

    overiding assignment completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
-
+import java.util.*;
 public class Main {
+    public static class A {
+        public String meth() {
+            System.out.println("A is called");
+          return  "Invoking method from class A";
+        }
+
+    }
+    public static class B extends A{
+            public String meth(){
+                System.out.println("B is called");
+               return "Method is overridden in Extendend class B";
+            }
+        }
+    public static void main(String[] args) {
+         B obj =new B();
+         obj.meth();
+        }
+
   
 }
